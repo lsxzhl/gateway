ee3bc3b5af7e604ac487b97f1b9759961eff8f6f
zhanghailong <zhanghailong@bitnei.cn>
2018-11-20 16:33:49 +0800

    gataway

---
+++ gateway/src/main/java/com/zhl/gateway/GatewayApplication.java
@@ -0,0 +1,27 @@
+package com.zhl.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder){
+        return builder.routes()
+                .route(t -> t.path("/hello")
+                .and()
+                .uri("http://localhost:8080"))
+                .build();
+
+    }
+
+
+}
---
+++ service1/src/main/java/com/zhl/service1/Service1Application.java
@@ -0,0 +1,24 @@
+package com.zhl.service1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Service1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Service1Application.class, args);
+    }
+
+
+    @GetMapping("/hello")
+    public String hello(){
+        System.out.println("调用hello方法。。。。");
+        return "Hello";
+    }
+
+
+}
